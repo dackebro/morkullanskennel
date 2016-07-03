23e82f639c5cc6ff51d7e0414ffcd4f1bad5b7e0
Erik Dackebro <erik.dackebro@gmail.com>
2016-07-03 13:46:55 +0200

    Now applies correct styling to image groups

--- frontend_resources/js/imgItem.jsx
+++ frontend_resources/js/imgItem.jsx
@@ -3,21 +3,61 @@ function ImgItem(props) {
   var imgs = props.imgCont.imgs;
   var path = props.path;
   var imgindex;
-  console.log(path);
 
   return (
     <div className="imgContainer">
-      {
-        imgs.map((img, index) => (
-          <span className={"thumb5imgs" +
-            (index == 0 ? " thumb5imgsleft" : (index == 4 ? " thumb5imgsright" : ""))
-          }>
-            <img src={'/imgs' + path + '/' + img + '.png'}/>
-          </span>
-        ))
-      }
+      {thumb5imgs(imgCount, imgs, path)}
+      {thumb3imgs(imgCount, imgs, path)}
+      {thumb2imgs(imgCount, imgs, path)}
+      {thumb1imgs(imgCount, imgs, path)}
     </div>
   );
+}
+
+function thumb5imgs(imgCount, imgs, path) {
+  if (imgCount == 5) {
+    return (imgs.map((img, index) => (
+      <span className={"thumb5imgs" +
+        (index == 0 ? " thumb5imgsleft" : (index == 4 ? " thumb5imgsright" : ""))
+      }>
+        <img src={'/imgs' + path + '/' + img + '.png'}/>
+      </span>
+    )))
+  }
+}
+
+function thumb3imgs(imgCount, imgs, path) {
+  if (imgCount == 3) {
+    return (imgs.map((img, index) => (
+      <span className={"thumb3imgs" +
+        (index == 0 ? " thumb3imgsleft" : (index == 2 ? " thumb2imgsright" : ""))
+      }>
+        <img src={'/imgs' + path + '/' + img + '.png'}/>
+      </span>
+    )))
+  }
+}
+
+function thumb2imgs(imgCount, imgs, path) {
+  if (imgCount == 2) {
+    return (imgs.map((img, index) => (
+      <span className={"thumb2imgs" +
+        (index == 0 ? " thumb2imgsleft" : (index == 1 ? " thumb2imgsright" : ""))
+      }>
+        <img src={'/imgs' + path + '/' + img + '.png'}/>
+      </span>
+    )))
+  }
+}
+
+function thumb1imgs(imgCount, imgs, path) {
+  if (imgCount == 1) {
+    return (imgs.map((img, index) => (
+      <span className={"thumb1imgs"}>
+        <img src={'/imgs' + path + '/' + img + '.png'}/>
+      </span>
+    )))
+  }
 }
 
 module.exports = ImgItem;
